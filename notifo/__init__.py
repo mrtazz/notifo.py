ec7fc76ad0934505e0733d4bdccfc75766394e36
Daniel Schauenberg <d@unwiredcouch.com>
2010-06-17 00:33:09 +0200

    add methods to directly utilize notifo

--- notifo/__init__.py
+++ notifo/__init__.py
@@ -5,3 +5,9 @@ from notifo import Notifo
 __author__ = "Daniel Schauenberg"
 __version__ = "0.1.0"
 __license__ = "MIT"
+
+def send_notification(login, pw, **kwargs):
+    return Notifo(login, pw).send_notification(**kwargs)
+
+def subscribe_user(login, pw, user):
+    return Notifo(login, pw).subscribe_user(user)
