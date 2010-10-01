5692b52171b8a25e4c9a2a9a459ded76dc77268b
Daniel Schauenberg <d@unwiredcouch.com>
2010-10-01 09:31:53 +0200

    add banned provider to message unit test

--- tests/test_notifo_message.py
+++ tests/test_notifo_message.py
@@ -9,12 +9,14 @@ class TestNotifyUser(unittest.TestCase):
 
     def setUp(self):
         self.provider = "test_provider"
+        self.provider_banned = "test_provider_msg_banned"
         self.user = "test_user"
         self.sender = "test_user2"
         self.banned = "test_user_banned"
         self.banned_token = "x128302fd34a60bf7e5670d003d858e6fb06ce6bf"
         self.sender_token = "x633a05b18f7f65bf461ffb3900c6eb70eaafb0ed"
         self.provider_token = "74515bc044df6594fbdb761b12a42f8028e14588"
+        self.provider_banned_token = "e34e447385fb4ff9084204cba19731d29c2afd78"
         self.user_token = "xbb8b3cba22a5f3d64fd404a07e84cdbb0c3566e5"
 
     def test_message(self):
@@ -32,6 +34,15 @@ class TestNotifyUser(unittest.TestCase):
                                 to=self.user, msg="foo test")
         self.assertEqual(1102, res["response_code"])
 
+    def test_message_provider(self):
+        res = send_message(self.provider, self.provider_token,
+                                to=self.user, msg="foo test")
+        self.assertEqual(2201, res["response_code"])
+
+    def test_message_provider_banned(self):
+        res = send_message(self.provider_banned, self.provider_banned_token,
+                                to=self.user, msg="foo test")
+        self.assertEqual(1102, res["response_code"])
 
 if __name__ == '__main__':
     unittest.main()
